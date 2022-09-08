1c8dc28d6f54e6135c418a1d758f82ae2ad968ab
GoGI321 <you@example.com>
2022-09-08 20:26:45 +0300

    project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import classes from './App.css';
 function App() {
   return (
-    <div className="App">
+    <div className={classes.appAdd}>
       Работает
     </div>
   );
